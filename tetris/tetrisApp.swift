4f87df643138e9a0ba66545dfb14f06b1fb7b969
Rishi <rishi_j@icloud.com>
2026-09-01 11:22:35 +0100

    Initial Commit

---
+++ tetris/tetrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  tetrisApp.swift
+//  tetris
+//
+//  Created by Rishi Jansari on 01/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct tetrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
